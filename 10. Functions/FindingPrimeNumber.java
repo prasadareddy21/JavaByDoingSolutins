48925d829e693c1ec77e15e0319c0465f72ec89b
Prasad <prasadareddy21@gmail.com>
2018-07-10 13:25:32 -0400

    Solved Finding Prime Numbers Problem

---
+++ 10. Functions/FindingPrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class FindingPrimeNumber{
+	
+	public static void main(String[] args){
+		
+		for (int j = 2; j <= 20; j++){
+			
+			if(isPrime(j)){
+				System.out.println(j + " <");
+			}else{
+				System.out.println(j);
+			}
+		}
+	}
+	
+	
+	public static boolean isPrime(int n){
+		
+		for (int i = 2; i < n ; i++){
+			if (n % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+}
